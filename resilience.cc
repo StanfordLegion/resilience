48c72ead9e2626d122c170df97bb22cba91f1d42
rupanshusoi <rupanshusoi@gmail.com>
2022-10-19 00:55:46 -0700

    Make state work (albeit with API modification)

--- resilience.cc
+++ resilience.cc
@@ -1,9 +1,13 @@
+#include <cassert>
 #include "legion.h"
 
 namespace ResilientLegion {
 
+class ResilientRuntime;
+
 class ResilientFuture {
  public:
+  int tag;
   Legion::Future lft;
 
   ResilientFuture(Legion::Future lft_) : lft(lft_) {}
@@ -11,10 +15,14 @@ class ResilientFuture {
   ResilientFuture() : lft(Legion::Future()) {}
 
   template<typename T>
-  inline T get_result(bool silence_warnings = false,
-                      const char *warning_string = NULL) const
+  inline T get_result(std::vector<std::pair<ResilientFuture, void *>> state_fts) const
   {
-    return lft.get_result<T>();
+    T result = lft.get_result<T>();
+    T *cpy = new T;
+    *cpy = result;
+    assert(state_fts.at(tag).second == nullptr);
+    state_fts[tag].second = cpy;
+    return result;
   }
 };
 
@@ -22,14 +30,23 @@ class ResilientRuntime {
  public:
   ResilientRuntime(Legion::Runtime *lrt_) : lrt(lrt_) {}
 
+  // Leaky
+  std::vector<std::pair<ResilientFuture, void *>> state_fts;
+
   ResilientFuture execute_task(Legion::Context ctx, Legion::TaskLauncher launcher) {
-    return lrt->execute_task(ctx, launcher);
+    ResilientFuture ft = lrt->execute_task(ctx, launcher);
+    ft.tag = state_fts.size();
+    state_fts.push_back(std::make_pair(ft, nullptr));
+    return ft;
   }
 
   ResilientFuture get_current_time(Legion::Context ctx,
                                    ResilientFuture precondition = ResilientFuture())
   {
-    return lrt->get_current_time(ctx, precondition.lft);
+    ResilientFuture ft = lrt->get_current_time(ctx, precondition.lft);
+    ft.tag = state_fts.size();
+    state_fts.push_back(std::make_pair(ft, nullptr));
+    return ft;
   }
 
  private:
