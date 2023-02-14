95130e43e9d4d4dc7aef3de84a72aaa0aabc6fb1
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-02-14 10:57:41 -0800

    Test script.

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+set -x
+
+git submodule update --init
+
+if [[ ! -e legion ]]; then
+    git clone -b control_replication https://gitlab.com/StanfordLegion/legion.git
+fi
+
+pushd legion
+if [[ ! -e build ]]; then
+    mkdir build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$PWD/../install -DCMAKE_CXX_STANDARD=11 ..
+    make install -j${THREADS:-4}
+fi
+popd
+
+mkdir -p build
+pushd build
+cmake -DCMAKE_PREFIX_PATH=$PWD/../legion/install ..
+make -j${THREADS:-4}
+popd
