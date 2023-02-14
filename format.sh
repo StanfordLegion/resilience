fd1fc961e222fa4cd0c4393f97273b6ac7087f30
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-02-14 11:54:28 -0800

    Check format in CI.

--- format.sh
+++ format.sh
@@ -5,3 +5,10 @@ set -e
 for f in src/*.cc src/*.h tests/*.cc; do
     clang-format -i "$f"
 done
+
+if [[ $CHECK_FORMAT -eq 1 ]]; then
+    set -x
+    git status
+    git diff
+    git diff-index --quiet HEAD
+fi
