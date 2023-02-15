a6133474c98a8c7dbb93781126117b6c1216805e
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-02-15 11:21:06 -0800

    Run test with abort.

--- tests/runner.sh
+++ tests/runner.sh
@@ -11,6 +11,28 @@ set -x
 "$@"
 
 # Make sure every checkpoint is individually replayable.
-for checkpoint in *.dat; do
-  "$@" -replay -cpt $(echo "$checkpoint" | cut -d. -f2)
-done
+if compgen -G '*.dat' > /dev/null; then
+    for checkpoint in *.dat; do
+        "$@" -replay -cpt $(echo "$checkpoint" | cut -d. -f2)
+    done
+else
+    echo "No checkpoints in normal run"
+    exit 1
+fi
+
+if [[ $1 = */region ]]; then
+    echo "region fails abort test, skipping"
+    exit 0
+fi
+
+rm -f *.dat
+
+# Run with abort (if supported).
+"$@" -abort || true
+
+# Make sure every checkpoint is individually replayable.
+if compgen -G '*.dat' > /dev/null; then
+    for checkpoint in *.dat; do
+        "$@" -replay -cpt $(echo "$checkpoint" | cut -d. -f2)
+    done
+fi
