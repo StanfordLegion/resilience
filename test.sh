afdf92fe82a115c66cb3d675d2864c3026f38518
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-03-02 11:31:18 -0800

    Add Regent build.

--- test.sh
+++ test.sh
@@ -6,6 +6,9 @@ set -x
 if [[ $INSTALL_DEPS -eq 1 ]]; then
     $SUDO_COMMAND apt-get update -qq
     $SUDO_COMMAND apt-get install -qq mpich libmpich-dev
+    if [[ ${USE_REGENT:-0} -eq 1 ]]; then
+        $SUDO_COMMAND apt-get install -qq llvm-11-dev clang-11 libclang-11-dev
+    fi
 fi
 
 git submodule update --init
@@ -17,7 +20,7 @@ fi
 pushd legion
 if [[ ! -e build ]]; then
     mkdir build
-    cd build
+    pushd build
     legion_flags=(
         -DCMAKE_BUILD_TYPE=$([ ${DEBUG:-1} -eq 1 ] && echo Debug || echo Release)
         -DCMAKE_INSTALL_PREFIX=$PWD/../install
@@ -31,6 +34,11 @@ if [[ ! -e build ]]; then
             -DBUILD_MARCH= # to avoid -march=native for valgrind compatibility
         )
     fi
+    if [[ ${USE_REGENT:-0} -eq 1 ]]; then
+        legion_flags+=(
+            -DLegion_BUILD_BINDINGS=ON
+        )
+    fi
     if [[ -n $LEGION_NETWORKS ]]; then
         legion_flags+=(
             -DLegion_NETWORKS=$LEGION_NETWORKS
@@ -38,6 +46,12 @@ if [[ ! -e build ]]; then
     fi
     cmake "${legion_flags[@]}" ..
     make install -j${THREADS:-4}
+    popd
+    if [[ ${USE_REGENT:-0} -eq 1 ]]; then
+        pushd language
+        ./install.py --legion-install-prefix=$PWD/../install --rdir=auto
+        popd
+    fi
 fi
 popd
 
@@ -57,5 +71,9 @@ if [[ -n $LEGION_NETWORKS ]]; then
 fi
 cmake "${resilience_flags[@]}" ..
 make -j${THREADS:-4}
-REALM_SYNTHETIC_CORE_MAP= ctest --output-on-failure -j${THREADS:-4}
+if [[ ${USE_REGENT:-0} -eq 1 ]]; then
+    echo "FIXME: no Regent tests for now"
+else
+    REALM_SYNTHETIC_CORE_MAP= ctest --output-on-failure -j${THREADS:-4}
+fi
 popd
