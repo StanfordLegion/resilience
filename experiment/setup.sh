7742ced02a2d43a81ceb247e3c025935f5784e98
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-03-03 11:00:47 -0800

    Experiment setup script.

---
+++ experiment/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+root_dir="$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
+
+git submodule update --init
+
+if [[ ! -e legion ]]; then
+    git clone -b regent-resilience https://gitlab.com/StanfordLegion/legion.git
+fi
+
+pushd legion/language
+# setup_env.py pins everything now, so don't need to pin explicitly here
+DEBUG=0 CC=cc CXX=CC HOST_CC=gcc HOST_CXX=g++ USE_GASNET=1 REALM_NETWORKS=gasnetex "$root_dir"/scripts/setup_env.py --cmake --extra='-DCMAKE_INSTALL_PREFIX=$PWD/../install'
+popd
+
+mkdir -p build
+pushd build
+resilience_flags=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_PREFIX_PATH=$PWD/../legion/install
+    -DCMAKE_CXX_FLAGS="-Wall -DRESILIENCE_AUDIT_FUTURE_API"
+    # do NOT set NDEBUG, it causes all sorts of issues
+    -DCMAKE_CXX_FLAGS_RELEASE="-O2 -march=native"
+)
+cmake "${resilience_flags[@]}" ..
+make -j${THREADS:-4}
+popd
