7e40c80189b2319dfaac1982472e22090a5626cd
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-03-03 23:30:18 +0100

    Fix experiment build scripts.

--- experiment/build_stencil.sh
+++ experiment/build_stencil.sh
@@ -4,9 +4,15 @@ set -e
 
 root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
+export INCLUDE_PATH="$root_dir/../src"
+export LIB_PATH="$root_dir/../build/src"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$root_dir/../build/src"
+
 mkdir "$1"
 cd "$1"
 
-USE_FOREIGN=0 SAVEOBJ=1 STANDALONE=1 OBJNAME=./stencil.checkpoint $root_dir/../regent.py $root_dir/../examples/stencil_fast.rg -fflow 0 -fopenmp 0 -foverride-demand-cuda 1 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal -fcheckpoint 1
+USE_FOREIGN=0 SAVEOBJ=1 STANDALONE=1 OBJNAME=./stencil.checkpoint $root_dir/../legion/language/regent.py $root_dir/../legion/language/examples/stencil_fast.rg -fflow 0 -fopenmp 0 -foverride-demand-cuda 1 -fcuda 1 -fcuda-offline 1 -fcuda-arch pascal -fcheckpoint 1
+
+cp $root_dir/../build/src/liblegion_resilience.so .
 
 cp $root_dir/*_stencil*.sh .
