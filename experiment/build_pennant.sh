2b0d17ad5f25cea6326fec399364f71ab6babb37
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-03-03 16:12:58 -0800

    Add Pennant test configurations.

--- experiment/build_pennant.sh
+++ experiment/build_pennant.sh
@@ -16,3 +16,4 @@ SAVEOBJ=1 STANDALONE=1 OBJNAME=./pennant.checkpoint $root_dir/../legion/language
 cp $root_dir/../build/src/liblegion_resilience.so .
 
 cp $root_dir/*_pennant*.sh .
+cp -r $root_dir/pennant.tests .
