5c941b46d1439a937a08a7ab34e61edb0ba43938
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-03-07 13:54:11 -0800

    Update experiment scripts.

--- experiment/sbatch_pennant.sh
+++ experiment/sbatch_pennant.sh
@@ -15,13 +15,13 @@ if [[ ! -d checkpoint ]]; then mkdir checkpoint; fi
 pushd checkpoint
 
 for n in $SLURM_JOB_NUM_NODES; do
-  for freq in 100 30 10 3 1; do
+  for freq in 300 100 30 10; do
     for r in 0 1 2 3 4; do
       slug="${n}x1_f${freq}_r${r}"
       echo "Running $slug"
       checkpoint_dir="$SCRATCH/$experiment_name/$slug"
       mkdir -p "$checkpoint_dir"
-      srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/pennant.checkpoint" "$root_dir"/pennant.tests/leblanc_long"$(( n * 4 ))"x30/leblanc.pnt -npieces "$n" -numpcx 1 -numpcy "$n" -seq_init 0 -par_init 1 -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 2 -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -lg:eager_alloc_percentage 10 -level 3 -lg:no_tracing -checkpoint:prefix "$checkpoint_dir" -checkpoint:auto_steps $freq -checkpoint:skip_leak_check | tee out_"$slug".log
+      srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/pennant.checkpoint" "$root_dir"/pennant.tests/leblanc_long"$(( n * 4 ))"x30/leblanc.pnt -npieces "$n" -numpcx 1 -numpcy "$n" -seq_init 0 -par_init 1 -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:io 1 -ll:util 2 -ll:bgwork 4 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -lg:eager_alloc_percentage 10 -level 3 -lg:no_physical_tracing -checkpoint:prefix "$checkpoint_dir" -checkpoint:auto_steps $freq -checkpoint:skip_leak_check | tee out_"$slug".log
       # -dm:memoize -lg:parallel_replay 2
     done
   done
@@ -37,7 +37,7 @@ for n in $SLURM_JOB_NUM_NODES; do
     freq=0
     slug="${n}x1_f${freq}_r${r}"
     echo "Running $slug"
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/pennant.checkpoint" "$root_dir"/pennant.tests/leblanc_long"$(( n * 4 ))"x30/leblanc.pnt -npieces "$n" -numpcx 1 -numpcy "$n" -seq_init 0 -par_init 1 -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 2 -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -lg:eager_alloc_percentage 10 -level 3 -lg:no_tracing -checkpoint:disable | tee out_"$slug".log
+    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/pennant.checkpoint" "$root_dir"/pennant.tests/leblanc_long"$(( n * 4 ))"x30/leblanc.pnt -npieces "$n" -numpcx 1 -numpcy "$n" -seq_init 0 -par_init 1 -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:io 1 -ll:util 2 -ll:bgwork 4 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -lg:eager_alloc_percentage 10 -level 3 -lg:no_physical_tracing -checkpoint:disable | tee out_"$slug".log
     # -dm:memoize -lg:parallel_replay 2
   done
 done
--- experiment/sbatch_stencil.sh
+++ experiment/sbatch_stencil.sh
@@ -21,13 +21,13 @@ for i in $power; do
   n=$(( 2 ** i))
   nx=$(( 2 ** ((i+1)/2) ))
   ny=$(( 2 ** (i/2) ))
-  for freq in 100 30 10 3 1; do
+  for freq in 300 100 30 10; do
     for r in 0 1 2 3 4; do
       slug="${n}x1_f${freq}_r${r}"
       echo "Running $slug"
       checkpoint_dir="$SCRATCH/$experiment_name/$slug"
       mkdir -p "$checkpoint_dir"
-      srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.checkpoint" -nx $(( nx * 20000 )) -ny $(( ny * 20000 )) -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 1 -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -lg:eager_alloc_percentage 10 -level 3 -lg:no_tracing -checkpoint:prefix "$checkpoint_dir" -checkpoint:auto_steps $freq -checkpoint:skip_leak_check | tee out_"$slug".log
+      srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.checkpoint" -nx $(( nx * 20000 )) -ny $(( ny * 20000 )) -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:io 1 -ll:util 1 -ll:bgwork 4 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -lg:eager_alloc_percentage 10 -level 3 -lg:no_physical_tracing -checkpoint:prefix "$checkpoint_dir" -checkpoint:auto_steps $freq -checkpoint:skip_leak_check | tee out_"$slug".log
       # -dm:memoize -lg:parallel_replay 2
     done
   done
@@ -46,7 +46,7 @@ for i in $power; do
     freq=0
     slug="${n}x1_f${freq}_r${r}"
     echo "Running $slug"
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.checkpoint" -nx $(( nx * 20000 )) -ny $(( ny * 20000 )) -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:util 1 -ll:bgwork 2 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -lg:eager_alloc_percentage 10 -level 3 -lg:no_tracing -checkpoint:disable | tee out_"$slug".log
+    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/stencil.checkpoint" -nx $(( nx * 20000 )) -ny $(( ny * 20000 )) -ntx $(( nx )) -nty $(( ny )) -tsteps 50 -tprune 30 -hl:sched 1024 -ll:gpu 1 -ll:io 1 -ll:util 1 -ll:bgwork 4 -ll:csize 15000 -ll:fsize 15000  -ll:rsize 512 -ll:gsize 0 -lg:eager_alloc_percentage 10 -level 3 -lg:no_physical_tracing -checkpoint:disable | tee out_"$slug".log
     # -dm:memoize -lg:parallel_replay 2
   done
 done
