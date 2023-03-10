274981e96bd04bf6b5cc9bbb95086873fe0e1609
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-03-10 09:51:00 -0800

    Fix library copy.

--- experiment/build_cpp_circuit.sh
+++ experiment/build_cpp_circuit.sh
@@ -7,7 +7,7 @@ root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 mkdir "$1"
 cd "$1"
 
-cp $root_dir/../legion/install/lib*/liblegion.so* .
+cp $root_dir/../legion/install/lib*/*.so* .
 cp $root_dir/../build/examples/circuit/circuit .
 
 cp $root_dir/*_cpp_circuit*.sh .
