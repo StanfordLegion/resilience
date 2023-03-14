9026444119b1d74a6da78b4c6ff89c625a03e93e
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-03-14 09:55:16 -0700

    Run 350 timesteps of Circuit.

--- experiment/sbatch_cpp_circuit.sh
+++ experiment/sbatch_cpp_circuit.sh
@@ -21,7 +21,7 @@ for n in $SLURM_JOB_NUM_NODES; do
       echo "Running $slug"
       checkpoint_dir="$SCRATCH/$experiment_name/$slug"
       mkdir -p "$checkpoint_dir"
-      srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/circuit" -npp 5000 -wpp 20000 -pct 98 -l 300 -p $n -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:io 1 -ll:util 2 -ll:bgwork 4 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -lg:eager_alloc_percentage 10 -lg:no_tracing -level 3 -logfile log_"$slug"_%.log -checkpoint:prefix "$checkpoint_dir" -checkpoint:auto_steps $freq | tee out_"$slug".out
+      srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/circuit" -npp 5000 -wpp 20000 -pct 98 -l 350 -p $n -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:io 1 -ll:util 2 -ll:bgwork 4 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -lg:eager_alloc_percentage 10 -lg:no_tracing -level 3 -logfile log_"$slug"_%.log -checkpoint:prefix "$checkpoint_dir" -checkpoint:auto_steps $freq | tee out_"$slug".out
       # -dm:memoize -lg:parallel_replay 2
 
       # Clean up frequent checkpoints, otherwise we use too much space
@@ -42,7 +42,7 @@ for n in $SLURM_JOB_NUM_NODES; do
     freq=0
     slug="${n}x1_f${freq}_r${r}"
     echo "Running $slug"
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/circuit" -npp 5000 -wpp 20000 -pct 98 -l 300 -p $n -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:io 1 -ll:util 2 -ll:bgwork 4 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -lg:eager_alloc_percentage 10 -lg:no_tracing -level 3 -logfile log_"$slug"_%.log -checkpoint:disable | tee out_"$slug".out
+    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/circuit" -npp 5000 -wpp 20000 -pct 98 -l 350 -p $n -prune 30 -hl:sched 1024 -ll:gpu 1 -ll:io 1 -ll:util 2 -ll:bgwork 4 -ll:csize 15000 -ll:fsize 15000 -ll:zsize 2048 -ll:rsize 512 -ll:gsize 0 -lg:eager_alloc_percentage 10 -lg:no_tracing -level 3 -logfile log_"$slug"_%.log -checkpoint:disable | tee out_"$slug".out
     # -dm:memoize -lg:parallel_replay 2
   done
 done
