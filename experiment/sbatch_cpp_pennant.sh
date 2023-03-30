8445e599c79d2a5e7816c80717b0420018f2fd13
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-03-30 07:45:39 +0200

    Update C++ Pennant script.

--- experiment/sbatch_cpp_pennant.sh
+++ experiment/sbatch_cpp_pennant.sh
@@ -17,19 +17,19 @@ if [[ ! -d checkpoint ]]; then mkdir checkpoint; fi
 pushd checkpoint
 
 for n in $SLURM_JOB_NUM_NODES; do
-  for freq in 1000 300 100 30; do
+  for freq in 1000 300 100; do
     for r in 0 1 2 3 4; do
       slug="${n}x1_f${freq}_r${r}"
       echo "Running $slug"
       checkpoint_dir="$SCRATCH/$experiment_name/$slug"
       mkdir -p "$checkpoint_dir"
-      srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/pennant" -n "$n" -f "$root_dir"/pennant.tests/leblanc_long"$(( n * 4 ))"x1000/leblanc.pnt -hl:sched 1024 -ll:gpu 1 -ll:io 1 -ll:util 2 -ll:bgwork 4 -ll:csize 12000 -ll:fsize 12000 -ll:zsize 36000 -ll:rsize 0 -ll:gsize 0 -lg:eager_alloc_percentage 10 -lg:no_tracing -level 3 -logfile log_"$slug"_%.log -checkpoint:prefix "$checkpoint_dir" -checkpoint:auto_steps $freq | tee out_"$slug".out
+      srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/pennant" -n "$n" -f "$root_dir"/pennant.tests/leblanc_long"$(( n * 4 ))"x1000/leblanc.pnt -hl:sched 1024 -ll:gpu 1 -ll:io 1 -ll:util 2 -ll:bgwork 4 -ll:csize 13000 -ll:fsize 13000 -ll:zsize 36000 -ll:rsize 0 -ll:gsize 0 -lg:eager_alloc_percentage 5 -lg:no_tracing -level 3 -logfile log_"$slug"_%.log -checkpoint:prefix "$checkpoint_dir" -checkpoint:auto_steps $freq | tee out_"$slug".out
       # -dm:memoize -lg:parallel_replay 2
 
       # Clean up frequent checkpoints, otherwise we use too much space
-      if (( freq < 1000 )); then
+      # if (( freq < 1000 )); then
           rm -rf "$checkpoint_dir"
-      fi
+      # fi
     done
   done
 done
@@ -44,7 +44,7 @@ for n in $SLURM_JOB_NUM_NODES; do
     freq=0
     slug="${n}x1_f${freq}_r${r}"
     echo "Running $slug"
-    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/pennant" -n "$n" -f "$root_dir"/pennant.tests/leblanc_long"$(( n * 4 ))"x1000/leblanc.pnt -hl:sched 1024 -ll:gpu 1 -ll:io 1 -ll:util 2 -ll:bgwork 4 -ll:csize 12000 -ll:fsize 12000 -ll:zsize 36000 -ll:rsize 0 -ll:gsize 0 -lg:eager_alloc_percentage 10 -lg:no_tracing -level 3 -logfile log_"$slug"_%.log -checkpoint:disable | tee out_"$slug".out
+    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none "$root_dir/pennant" -n "$n" -f "$root_dir"/pennant.tests/leblanc_long"$(( n * 4 ))"x1000/leblanc.pnt -hl:sched 1024 -ll:gpu 1 -ll:io 1 -ll:util 2 -ll:bgwork 4 -ll:csize 13000 -ll:fsize 13000 -ll:zsize 36000 -ll:rsize 0 -ll:gsize 0 -lg:eager_alloc_percentage 5 -lg:no_tracing -level 3 -logfile log_"$slug"_%.log -checkpoint:disable | tee out_"$slug".out
     # -dm:memoize -lg:parallel_replay 2
   done
 done
